f54c6ce49a911117f7aea5f0235a35324797efaa
unknown <longfeiz@umich.edu>
2023-12-14 16:37:36 -0500

    optimize code to generate first two figures

--- source/preprocessing.R
+++ source/preprocessing.R
@@ -10,62 +10,31 @@ mydata$mydate <- unclass(as.Date(mydata$Date, format = '%m/%d/%Y'))
 mydata$mydate <- mydata$mydate-min(mydata$mydate)+1
 
 
-## calculate profit
+## derive item summary data
 mydata <- mydata%>%
   group_by(Item.Number) %>%
   mutate(State.Bottle.Retail=mean(State.Bottle.Retail)) %>%
   mutate(State.Bottle.Cost=mean(State.Bottle.Cost)) %>%
-  mutate(Profit=(State.Bottle.Retail-State.Bottle.Cost)*Bottles.Sold)
-
-
-## calculate total profit for each item
-total_profit <- mydata %>% group_by(Item.Number) %>%
-  mutate(tol_profit=sum(Profit)/1e6) %>%
   mutate(botl_profit=State.Bottle.Retail-State.Bottle.Cost) %>%
-  distinct(Item.Number, tol_profit, botl_profit, State.Bottle.Cost) %>%
   filter(botl_profit>0) %>%
   filter(botl_profit<60) %>%
-  arrange(desc(tol_profit), desc(botl_profit))
+  mutate(Profit=botl_profit*Bottles.Sold) %>%
+  mutate(tol_profit=sum(Profit)/1e6) %>%
+  mutate(week=rep(1:209, each=7)[mydate]) %>%
+  mutate(week_profit=sum(Profit))
+  
 
-## save total_profit as csv file
-write_csv(total_profit, "derived_data/total_profit.csv")
+#derive profit data
+profit <- mydata %>%
+  distinct(Item.Number, tol_profit, week_profit, State.Bottle.Cost, week)
 
-
-## extract top 10 total profit
-top10 <- total_profit[1:10, ]
-
-
-## calculate profit by day
-profit_date <- mydata %>% group_by(Item.Number, mydate) %>%
-  mutate(p=sum(Profit)) %>%
-  distinct(Item.Number, mydate, p)
-
-
-## calculate weekly profit for each liquor in top 10
-trend <- matrix(0, nrow = 209, ncol = 11)
-trend[, 1] <- 1:209
-colnames(trend) <- c('week', top10$Item.Number)
-for(i in 1:10){
-  tmp <- profit_date %>%
-    filter(Item.Number==top10$Item.Number[i]) %>%
-    ungroup %>%
-    mutate(week=rep(1:209, each=7)[mydate]) %>%
-    add_row(week = 1:209, p = 0) %>%
-    group_by(week) %>% 
-    mutate(week_p = sum(p)/1e3) %>%
-    distinct(week, week_p) %>%
-    arrange(week)
-  trend[, i+1] <- as.vector(tmp$week_p)
-}
-trend <- as.data.frame(trend)
-
-## save trend as csv file
-write_csv(trend, "derived_data/trend.csv")
+## save profit as csv file
+write_csv(profit, "derived_data/profit.csv")
 
 
 
 
-
+  
 
 
 
--- source/profit_plot.R
+++ source/profit_plot.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 
 ## load profit vs cost data
-total_profit <- read_csv("derived_data/total_profit.csv")
+profit <- read_csv("derived_data/profit.csv")
 
 ## total profit against bottle cost plot
+total_profit <- profit %>%
+  distinct(Item.Number, tol_profit, State.Bottle.Cost) %>%
+  arrange(desc(tol_profit), desc(State.Bottle.Cost))
+
 profit_plot <- ggplot(total_profit, aes(State.Bottle.Cost, tol_profit)) + 
   geom_point(size = 1) + 
   labs(title = 'Total Profit Plot', 
@@ -12,3 +16,38 @@ profit_plot <- ggplot(total_profit, aes(State.Bottle.Cost, tol_profit)) +
 
 ## save plot
 ggsave(profit_plot, filename = "figures/fig1_profit_vs_cost.png", height = 6, width = 10)
+
+
+
+## extract top 10 total profit
+top10 <- unique(total_profit$Item.Number)[1:10]
+week_profit <- profit %>%
+  filter(Item.Number %in% top10) %>%
+  distinct(Item.Number, week, week_profit)
+
+## calculate weekly profit for each liquor in top 10
+trend <- matrix(0, nrow = 209, ncol = 11)
+trend[, 1] <- 1:209
+colnames(trend) <- c('week', top10)
+for(i in 1:10){
+  tmp <- week_profit %>%
+    ungroup() %>%
+    filter(Item.Number==top10[i]) %>%
+    select(week, week_profit) %>%
+    arrange(week)
+  trend[tmp$week, i+1] <- as.vector(tmp$week_profit)
+}
+trend <- as.data.frame(trend)
+
+
+## map trend for each top 10 liquor
+trend_long <- trend %>% pivot_longer(-week)
+trend_plot <- ggplot(trend_long) + 
+  geom_smooth(aes(week,value,colour=name), 
+              method = 'loess', se = FALSE, formula = y~x) +
+  labs(title = 'Top 10 Weekly Profit Trend', 
+       x = 'Week', 
+       y = 'Week Profit (kUSD)')
+
+## save plot
+ggsave(trend_plot, filename = "figures/fig2_top_10_trend_plot.png", height = 6, width = 10)
